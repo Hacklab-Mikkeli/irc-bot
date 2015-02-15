a8d166243e8b25a64a50a250d8fb754ddc2d51bd
Belvain <heikki.kurhinen@gmail.com>
2015-02-15 19:27:04 +0200

    ** Added example private message function

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var Bot = require('./bot/bot.js');
 
 var bot = new Bot(conf.server, conf.nick, conf.channel);
 
-bot.message('hello', function(from, to, message){
-  console.log('Hello function called!');
+bot.message('hello', function(from, to, text, raw){
   bot.say('Hello world!');
+});
+
+bot.pm('hello', function (from, text, raw) { 
+  bot.sendPm(from, 'Private hello');
 });
 
 bot.start();
