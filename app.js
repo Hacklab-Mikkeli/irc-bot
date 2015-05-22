d3dda417108f4ddc29d490a61a11a77765406316
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2015-05-22 21:11:36 +0300

    ** Added webhook listener

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var conf = require('./config');
 var Bot = require('./bot/bot.js');
 var webhook = require('./modules/mailchimp/webhookListener.js');
+var http = require('http');
 
 var bot = new Bot(conf.server, conf.nick, conf.channel);
 
@@ -15,5 +16,21 @@ bot.message('hello', function(from, to, text, raw){
 bot.pm('hello', function (from, text, raw) { 
   bot.sendPm(from, 'Private hello');
 });
+
+http.createServer(function (req, res) {
+	if(req.method == 'POST'){
+		var body = '';
+		req.on('data', function(data) {
+			body += data;
+			if (body.length > 1e6) // If body gets too large, kill the request.
+			req.connection.destroy(); // (In case someone is doing naughty things like sending endless post requests)
+		});
+		req.on('end', function (){
+		  bot.say('Hacklab door opened!');
+		  res.writeHead(200, { 'Content-Type': 'text/plain' });
+		  res.end('ok');
+		});
+	}
+}).listen(9051);
 
 bot.start();
