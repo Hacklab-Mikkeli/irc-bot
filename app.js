24c480fd1339e139d2e4d582a5efb5a254290497
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2015-08-20 01:30:23 +0300

    ** Bot will now post each link to reddit

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 var conf = require('./config');
 var Bot = require('./bot/bot.js');
 var webhook = require('./modules/mailchimp/webhookListener.js');
+var reddit = require('./modules/reddit/reddit.js');
 var http = require('http');
+var URI = require('URIjs');
 
 var bot = new Bot(conf.server, conf.nick, conf.channel);
 
 webhook.on('subscribe', function (data, meta) {
   bot.say('New subscriber joined to the mailing list!');
+});
+
+bot.onEachMessage(function(from, to, text, raw){
+	 var link = URI.withinString(text, function(url) {
+		if(url){
+			reddit.submitLink('hacklabmikkeli', url, url);
+		}
+		return false;
+	});
+	
 });
 
 bot.message('hello', function(from, to, text, raw){
